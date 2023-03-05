5f9e43fc559ad3522058492cf67c3f8b944c2d5e
Amauari Ulloa Galicia <ing.amaury06@gmail.com>
2023-03-05 16:39:32 -0400

    prueba 6

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
         <ContainerHeader>
         <ContainerButtons>
             <Button to="/categorias">Categorias</Button>
-            <Button to="/lista">Lista</Button>
+            <Button to="/listas">Lista</Button>
             <BtnLogout />
           </ContainerButtons>
           <Title> Agregar Gastos</Title>
